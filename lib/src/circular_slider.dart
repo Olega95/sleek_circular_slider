cdd74f394d732fcedf0cd8cdfa52cd39c3bcbcee
Oleg Korotin <o9818154818@yandex.ru>
2022-02-15 12:19:52 +0300

    Update circular_slider.dart

--- lib/src/circular_slider.dart
+++ lib/src/circular_slider.dart
@@ -148,7 +148,7 @@ class _SleekCircularSliderState extends State<SleekCircularSlider>
       _setupPainter();
     }
     return RawGestureDetector(
-        behavior: widget.isOpaque ? HitTestBehavior.opaque : widget.innerWidget != null ? HitTestBehavior.defetToChild : HitTestBehavior.translucent, 
+        behavior: widget.isOpaque ? HitTestBehavior.opaque : widget.innerWidget != null ? HitTestBehavior.deferToChild : HitTestBehavior.translucent, 
         gestures: <Type, GestureRecognizerFactory>{
           _CustomPanGestureRecognizer:
               GestureRecognizerFactoryWithHandlers<_CustomPanGestureRecognizer>(
